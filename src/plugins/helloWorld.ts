93b7636593050021648dc93cb44d343b5613e591
willocn <willocn.mc@gmail.com>
2021-08-18 01:17:57 -0500

    improve hello world example

--- src/plugins/helloWorld.ts
+++ src/plugins/helloWorld.ts
@@ -1,3 +1,4 @@
+import { PacketMeta } from "minecraft-protocol";
 import AbstractPlugin from "../abstractPlugin";
 import PluginManager from "../pluginManager";
 import ProxyServer from "../proxyServer";
@@ -8,9 +9,20 @@ export default class HelloWorldPlugin extends AbstractPlugin {
     constructor(proxyServer: ProxyServer, pluginManager: PluginManager) {
         super(proxyServer, pluginManager);
         this.commands["hello"] = this.helloCommand;
+        this.proxyServer.clientbound.once("login", this.handleLoginPacket); // register clientbound packet listener
     }
 
+    /*
+    Example command. arg parsing is left up to the plugin.
+    */
     helloCommand = (args: string[]): void => {
         this.sendChat("hai :3");
+    }
+
+    /*
+    Example packet listener, runs every time the login packet is sent
+    */
+    handleLoginPacket = (data: Record<string, any>, meta: PacketMeta): void => {
+        this.sendChat(`Logged in! :D eid: ${data.entityId}`);
     }
 }
