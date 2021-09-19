97576f79ee85659949d09e88c28c34d8f6dcf169
willocn <willocn.mc@gmail.com>
2021-09-19 00:39:03 -0500

    pretty

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import { readFileSync } from "fs";
 import ProxyServer from "./proxyServer.js";
 import sharp from "sharp";
+import chalk from "chalk";
+
+console.log(chalk.blue.bold("\ndoorman proxy"));
+console.log(chalk.white("by WillOCN\n"));
 
 const b64 = "data:image/png;base64," + (await sharp("images/logo.png").resize(64, 64).toBuffer()).toString("base64");
 
