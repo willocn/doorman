0d8d17c8836ccfe0b9267e1fb9b90fe041f3ffba
willocn <willocn.mc@gmail.com>
2021-09-19 02:19:19 -0500

    remove plugin index

--- src/plugins/index.ts
+++
@@ -1,6 +0,0 @@
-import HelloWorldPlugin from "./hello/helloWorld.js";
-import MetaPlugin from "./meta/metaPlugin.js";
-
-//TODO: export all in folder
-export {HelloWorldPlugin};
-export {MetaPlugin};
