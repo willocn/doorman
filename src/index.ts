9ea10374e81bb8824ebeb7cf107c400a34e25a6a
willocn <willocn.mc@gmail.com>
2021-09-18 21:15:30 -0500

    formatting nonsense

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import { readFileSync } from "fs";
 import ProxyServer from "./proxyServer.js";
 import sharp from "sharp";
-const b64 = "data:image/png;base64," + (await sharp("images/logo.png").resize(64, 64).toBuffer()).toString("base64");
 
+const b64 = "data:image/png;base64," + (await sharp("images/logo.png").resize(64, 64).toBuffer()).toString("base64");
 
 const p = new ProxyServer({
     destinationHost: "localhost",
     hostPort: 25569,
-    username: "robot",
+    username: "frankenstein",
     icon: b64
 });
